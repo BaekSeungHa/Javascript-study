614dc435cfbc5d749c892426b7af588075513a76
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-07-30 11:28:37 +0900

    객체

---
+++ js jquery/gakchae.js
@@ -0,0 +1,9 @@
+var member = {
+  name: "백승하",
+  email: "qortmdgk@naver.com",
+  phone: "010-6352-0876",
+};
+
+document.write(member.name + "<br>");
+document.write(member.email + "<br>");
+document.write(member.phone + "<br>");
